72b53daac82d6c2aa60183971d75026e776d80b2
Kyle Willard <kyleswillard@gmail.com>
2021-04-16 16:38:44 -0700

    Tons of stuff...

--- src/data.tsx
+++ src/data.tsx
@@ -8,6 +8,22 @@ const questions = [
     downvote: '3',
     author: { username: 'Bob Smith' },
     group: { groupName: 'Front End' },
+    responses: [
+      {
+        author: { username: 'Tim Jones' },
+        createdAt: 1519211809938,
+        resolution: 'Your Mom!',
+        upvote: '0',
+        downvote: '100',
+      },
+      {
+        author: { username: 'Mike Jones' },
+        createdAt: 1519211809938,
+        resolution: 'No! Your Mom!',
+        upvote: '0',
+        downvote: '100',
+      },
+    ],
   },
   {
     question: 'What is the meaning of life?',
@@ -18,6 +34,26 @@ const questions = [
     downvote: '3',
     author: { username: 'Angela Johnson' },
     group: { groupName: 'Back End' },
+  },
+  {
+    question: 'What is 1 - 1?',
+    createdAt: 1519211809934,
+    resolved: true,
+    resolution: '0',
+    upvote: '10',
+    downvote: '3',
+    author: { username: 'James Jones' },
+    group: { groupName: 'Front End' },
+  },
+  {
+    question: 'What is 1 + 3?',
+    createdAt: 1519211809934,
+    resolved: true,
+    resolution: '4',
+    upvote: '10',
+    downvote: '3',
+    author: { username: 'Amy Smith' },
+    group: { groupName: 'Front End' },
   },
 ];
 
